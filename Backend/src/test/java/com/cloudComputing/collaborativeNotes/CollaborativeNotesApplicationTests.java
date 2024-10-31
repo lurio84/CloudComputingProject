fd91902a879c7ed4754cb9fcdd42d8f806cfb263
lurio84 <115478617+lurio84@users.noreply.github.com>
2024-10-31 16:35:52 +0100

    Project initialized via Spring Initializr

---
+++ Backend/src/test/java/com/cloudComputing/collaborativeNotes/CollaborativeNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudComputing.collaborativeNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativeNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

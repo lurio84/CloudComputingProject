906103a93f6b1558d9c972021b181f159e863d67
lurio84 <lucas.ronquillo@gmail.com>
2024-11-20 17:42:33 +0100

    .env configuration

--- Backend/src/main/java/com/cloudComputing/collaborativeNotes/CollaborativeNotesApplication.java
+++ Backend/src/main/java/com/cloudComputing/collaborativeNotes/CollaborativeNotesApplication.java
@@ -1,12 +1,21 @@
 package com.cloudComputing.collaborativeNotes;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class CollaborativeNotesApplication {
 
 	public static void main(String[] args) {
+		// Load the .env file and set the environment variables
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DATABASE_URL", Objects.requireNonNull(dotenv.get("DATABASE_URL")));
+		System.setProperty("DATABASE_USERNAME", Objects.requireNonNull(dotenv.get("DATABASE_USERNAME")));
+		System.setProperty("DATABASE_PASSWORD", Objects.requireNonNull(dotenv.get("DATABASE_PASSWORD")));
+
 		SpringApplication.run(CollaborativeNotesApplication.class, args);
 	}
 

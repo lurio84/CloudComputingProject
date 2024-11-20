dda67c3e011e7f7c40aab1fed865c083f90165b5
lurio84 <lucas.ronquillo@gmail.com>
2024-11-20 13:29:59 +0100

    Database entities created and working

--- Backend/src/main/java/com/cloudComputing/collaborativeNotes/entities/ShareLink.java
+++ Backend/src/main/java/com/cloudComputing/collaborativeNotes/entities/ShareLink.java
@@ -1,4 +1,23 @@
 package com.cloudComputing.collaborativeNotes.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "share_link")
 public class ShareLink {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "note_id")
+    private Note note;
+
+    private String link;
+    private String accessLevel;
+    private LocalDateTime expirationDate;
+
+    // Getters y Setters
 }
--- Backend/src/main/java/com/cloudComputing/collaborativeNotes/entities/User.java
+++ Backend/src/main/java/com/cloudComputing/collaborativeNotes/entities/User.java
@@ -1,4 +1,17 @@
 package com.cloudComputing.collaborativeNotes.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+
+    // Getters y Setters
 }

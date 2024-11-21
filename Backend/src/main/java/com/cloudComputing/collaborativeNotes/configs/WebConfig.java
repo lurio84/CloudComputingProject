442467cc7ad8351f347aba94bc8c13da479dfb85
lurio84 <lucas.ronquillo@gmail.com>
2024-11-21 16:58:34 +0100

    Comments updated

--- Backend/src/main/java/com/cloudComputing/collaborativeNotes/configs/WebConfig.java
+++ Backend/src/main/java/com/cloudComputing/collaborativeNotes/configs/WebConfig.java
@@ -13,12 +13,11 @@ public class WebConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*"); // Permite todas las solicitudes de origen
-        config.addAllowedHeader("*"); // Permite todas las cabeceras
-        config.addAllowedMethod("*"); // Permite todos los métodos (GET, POST, etc.)
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+        config.setAllowCredentials(true); // Allow credentials such as cookies to be sent with requests
+        config.addAllowedOriginPattern("*"); // Allow all origins for cross-origin requests
+        config.addAllowedHeader("*"); // Allow all headers
+        config.addAllowedMethod("*"); // Allow all HTTP methods (GET, POST, etc.)
+        source.registerCorsConfiguration("/**", config); // Apply this CORS configuration to all endpoints
+        return new CorsFilter(source); // Return the CORS filter with the specified configuration
     }
 }
-

7d3d4c8d8c84f297cd64c306a6da9ea0a11cdd76
lurio84 <lucas.ronquillo@gmail.com>
2024-12-04 14:57:42 +0100

    User controller (Get and Post operations)

---
+++ Backend/src/main/java/com/cloudComputing/collaborativeNotes/controllers/UserController.java
@@ -0,0 +1,34 @@
+package com.cloudComputing.collaborativeNotes.controllers;
+
+import com.cloudComputing.collaborativeNotes.database.entities.User;
+import com.cloudComputing.collaborativeNotes.database.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUser(@PathVariable Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        try {
+            User savedUser = userRepository.save(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}

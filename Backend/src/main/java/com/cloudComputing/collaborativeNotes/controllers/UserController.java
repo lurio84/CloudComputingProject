69194271c85234b37d7d9973347a996cbd1c91b9
lurio84 <lucas.ronquillo@gmail.com>
2025-01-17 11:38:43 +0100

    Get all notes from a user endpoint

--- Backend/src/main/java/com/cloudComputing/collaborativeNotes/controllers/UserController.java
+++ Backend/src/main/java/com/cloudComputing/collaborativeNotes/controllers/UserController.java
@@ -1,11 +1,15 @@
 package com.cloudComputing.collaborativeNotes.controllers;
 
+import com.cloudComputing.collaborativeNotes.database.entities.Note;
 import com.cloudComputing.collaborativeNotes.database.entities.User;
+import com.cloudComputing.collaborativeNotes.database.repositories.NoteRepository;
 import com.cloudComputing.collaborativeNotes.database.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -13,6 +17,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private NoteRepository noteRepository;
 
     @GetMapping("/{userId}")
     public ResponseEntity<User> getUser(@PathVariable Long userId) {
@@ -30,4 +37,11 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+
+    @GetMapping("/{userId}/notes")
+    public ResponseEntity<List<Note>> getNotesByUserId(@PathVariable Long userId) {
+        List<Note> notes = noteRepository.findByUserId(userId);
+        return ResponseEntity.ok(notes);
+    }
+
 }
--- Backend/src/main/java/com/cloudComputing/collaborativeNotes/database/repositories/NoteRepository.java
+++ Backend/src/main/java/com/cloudComputing/collaborativeNotes/database/repositories/NoteRepository.java
@@ -2,6 +2,12 @@ package com.cloudComputing.collaborativeNotes.database.repositories;
 
 import com.cloudComputing.collaborativeNotes.database.entities.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
+    @Query("SELECT n FROM Note n JOIN n.userNotes un WHERE un.user.id = :userId")
+    List<Note> findByUserId(@Param("userId") Long userId);
 }

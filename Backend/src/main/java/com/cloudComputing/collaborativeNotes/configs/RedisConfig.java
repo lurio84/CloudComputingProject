da98b6c1d51fe15e7c10c8eb9c3a6ef4fa3d83c8
lurio84 <lucas.ronquillo@gmail.com>
2025-02-06 12:48:25 +0100

    Fixed RedisConfig to load env variables

--- Backend/src/main/java/com/cloudComputing/collaborativeNotes/configs/RedisConfig.java
+++ Backend/src/main/java/com/cloudComputing/collaborativeNotes/configs/RedisConfig.java
@@ -1,14 +1,43 @@
 package com.cloudComputing.collaborativeNotes.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.ssl:false}")
+    private boolean useTls;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        // Configuración de Redis
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+
+        // Configuración del cliente Lettuce con TLS si es necesario
+        LettuceClientConfiguration.LettuceClientConfigurationBuilder clientConfigBuilder =
+                LettuceClientConfiguration.builder();
+
+        if (useTls) {
+            clientConfigBuilder.useSsl(); // Se activa SSL/TLS si es necesario
+        }
+
+        LettuceClientConfiguration clientConfig = clientConfigBuilder.build();
+        return new LettuceConnectionFactory(config, clientConfig);
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {

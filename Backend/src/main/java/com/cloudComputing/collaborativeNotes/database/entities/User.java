0b2fec7d8f7e3d46cb34c417c1381b8f19d9a51e
lurio84 <lucas.ronquillo@gmail.com>
2025-02-13 22:01:21 +0100

    Usernames are unique

--- Backend/src/main/java/com/cloudComputing/collaborativeNotes/database/entities/User.java
+++ Backend/src/main/java/com/cloudComputing/collaborativeNotes/database/entities/User.java
@@ -11,7 +11,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(unique = true)
     private String username;
     private String email;
     private String password;

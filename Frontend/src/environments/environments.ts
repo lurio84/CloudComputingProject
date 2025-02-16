a46695adb73f9563e8b7703a4edd244c392c5e35
lurio84 <lucas.ronquillo@gmail.com>
2025-02-16 21:11:13 +0100

    Test deployment in localhost

--- Frontend/src/environments/environments.ts
+++ Frontend/src/environments/environments.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // apiUrl: window.location.origin + ':8080/',
-   apiUrl: 'http://localhost:8080/S',
+   apiUrl: 'http://localhost:8080/',
    websocketUrl:'http://localhost:8080/ws'
   // websocketUrl:window.location.origin + ':8080/ws'
 }
